50b264681034bfe4000512eeb49099372f714c7c
vep20 <vep20@inf.ufpr.br>
2023-05-30 16:09:42 -0300

    CERTO

---
+++ prep.T1/liblista_ordenada.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "liblista_ordenada.h"
+
+lista_t *lista_cria (){
+    
+}
+
+void lista_destroi (lista_t **l){
+
+}
+
+int lista_insere_ordenado (lista_t *l, elemento_t *elemento){
+
+}
+
+int lista_remove_ordenado (lista_t *l, elemento_t *elemento){
+
+}
---
+++ prep.T1/testa_lista_ordenada.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "liblista_ordenada.h" 
+
+
+int main(){
+
+}
