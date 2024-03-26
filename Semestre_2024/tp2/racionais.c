fa1da541d3d6460611abe44e7aa443926e8966a2
vep20 <vep20@inf.ufpr.br>
2024-03-26 14:03:42 -0300

    att 1.1

---
+++ Semestre_2024/tp2/racionais.c
@@ -0,0 +1,37 @@
+#include "racionais.h"
+/* acrescente demais includes que voce queira ou precise */
+
+/* Funcoes privadas restritas a este modulo.
+ * Voce deve implementa-las pois serao necessarias,
+ * embora elas nao precisem estar no .h */
+
+/* retorna um numero aleatorio entre min e max, inclusive. */
+int aleat (int min, int max){
+/* coloque o codigo aqui */
+}
+
+/* Maximo Divisor Comum entre a e b      */
+/* calcula o mdc pelo metodo de Euclides */
+/* dica: a versao recursiva eh bem elegante! */
+int mdc (int a, int b){
+/* coloque o codigo aqui */
+}
+
+/* Minimo Multiplo Comum entre a e b */
+/* mmc = (a * b) / mdc (a, b)        */
+int mmc (int a, int b){
+/* coloque o codigo aqui */
+}
+
+/* Recebe um numero racional e o retorna simplifico.
+ * Por exemplo, ao receber 10/8 devera retornar 5/4.
+ * Se ambos numerador e denominador forem negativos, devera retornar um positivo.
+ * Se o denominador for negativo, o sinal deve migrar para o numerador.
+ * Quem chama esta funcao deve garantir que o racional r eh valido */
+struct racional simplifica_r (struct racional r);
+
+/* aqui voce pode definir mais funcoes internas, caso queira ou precise */
+
+/*
+ * Implemente aqui as funcoes definidas no racionais.h.
+*/
