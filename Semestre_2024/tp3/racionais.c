838e68646a324df9e64d0dadea77a02c143e2921
vep20 <vep20@inf.ufpr.br>
2024-04-09 14:13:48 -0300

    Commit automático

---
+++ Semestre_2024/tp3/racionais.c
@@ -0,0 +1,34 @@
+#include "racionais.h"
+/* coloque demais includes aqui */
+
+/* Funcoes privadas restritas a este modulo.
+ * Voce deve implementa-las pois serao necessarias,
+ * embora elas nao precisem estar no .h */
+
+/* retorna um numero aleatorio entre min e max, inclusive. */
+int aleat (long int min, long int max){
+    /* coloque o codigo aqui */
+}
+
+/* Maximo Divisor Comum entre a e b.         */
+/* Calcula o mdc pelo metodo de Euclides.    */
+/* Dica: a versao recursiva eh bem elegante! */
+long int mdc (long int a, long int b){
+    /* coloque o codigo aqui */
+}
+
+/* Minimo Multiplo Comum entre a e b */
+/* mmc = (a * b) / mdc (a, b)        */
+long int mmc (long int a, long int b){
+    /* coloque o codigo aqui */
+}
+
+/* Recebe um numero racional e o retorna simplificado no parametro *r.
+   Quem chama esta funcao deve garantir que *r seja valido e nao NULL. */
+void simplifica_r (struct racional *r){
+    /* coloque o codigo aqui */
+}
+
+/* aqui voce pode definir mais funcoes internas, caso queira ou precise */
+
+/* Implemente aqui as funcoes cujos prototipos estao em racionais.h */
---
+++ Semestre_2024/tp3/tp3.c
@@ -0,0 +1,15 @@
+/* Arquivo MAIN que usa o TAD racionais */
+
+/* coloque seus includes aqui */
+
+/* coloque funcoes desejadas aqui. 
+ * Exemplo: ordenar, eliminar invalidos... */
+
+int main (){
+    /* vetor de ponteiros para racionais */
+    struct racional **v;  /* equivalente a struct racional *v[] */
+
+    /* coloque seu codigo aqui */
+
+    return 0;
+}
